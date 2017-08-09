7d3dc7513f5b0a910556ee42e646182d00bb1229
Badchoice <jordi@gloobus.net>
2017-08-09 20:07:20 +0200

    displaying reports as table

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -4,20 +4,51 @@
         <h3>Reports</h3>
     </div>
 
-    <div>
-        First Reply Time: {{ App\Kpi\FirstReplyKpi::forUser( auth()->user() ) }}
-    </div>
-
-    <div>
-        Solve Time: {{ App\Kpi\SolveKpi::forUser( auth()->user() ) }}
-    </div>
-
-    <div>
-        One touch resolution ratio: {{ App\Kpi\OneTouchResolutionKpi::forUser( auth()->user() ) }} %
-    </div>
-
-    <div>
-        Reopened ratio: {{ App\Kpi\ReopenedKpi::forUser( auth()->user() ) }} %
-    </div>
+    <table class="striped">
+        <thead>
+            <tr>
+                <th></th>
+                <th> First Reply Time </th>
+                <th> Solve Time </th>
+                <th> One touch resolution ratio </th>
+                <th> Reopened ratio </th>
+            </tr>
+        </thead>
+        <tr>
+            <td> You </td>
+            <td>  {{ App\Kpi\FirstReplyKpi::forUser( auth()->user() ) }}</td>
+            <td>  {{ App\Kpi\SolveKpi::forUser( auth()->user() ) }}        </td>
+            <td>  {{ App\Kpi\OneTouchResolutionKpi::forUser( auth()->user() ) }} %        </td>
+            <td>  {{ App\Kpi\ReopenedKpi::forUser( auth()->user() ) }} %        </td>
+        </tr>
+        <tr>
+            <td> You Previous month </td>
+            <td>  {{ App\Kpi\FirstReplyKpi::forUser( auth()->user() ) }}</td>
+            <td>  {{ App\Kpi\SolveKpi::forUser( auth()->user() ) }}        </td>
+            <td>  {{ App\Kpi\OneTouchResolutionKpi::forUser( auth()->user() ) }} %        </td>
+            <td>  {{ App\Kpi\ReopenedKpi::forUser( auth()->user() ) }} %        </td>
+        </tr>
+        <tr>
+            <td> Team 1 </td>
+            <td>  {{ App\Kpi\FirstReplyKpi::forUser( auth()->user() ) }}</td>
+            <td>  {{ App\Kpi\SolveKpi::forUser( auth()->user() ) }}        </td>
+            <td>  {{ App\Kpi\OneTouchResolutionKpi::forUser( auth()->user() ) }} %        </td>
+            <td>  {{ App\Kpi\ReopenedKpi::forUser( auth()->user() ) }} %        </td>
+        </tr>
+        <tr>
+            <td> Team 2 </td>
+            <td>  {{ App\Kpi\FirstReplyKpi::forUser( auth()->user() ) }}</td>
+            <td>  {{ App\Kpi\SolveKpi::forUser( auth()->user() ) }}        </td>
+            <td>  {{ App\Kpi\OneTouchResolutionKpi::forUser( auth()->user() ) }} %        </td>
+            <td>  {{ App\Kpi\ReopenedKpi::forUser( auth()->user() ) }} %        </td>
+        </tr>
+        <tr>
+            <td> All average </td>
+            <td>  {{ App\Kpi\FirstReplyKpi::forUser( auth()->user() ) }}</td>
+            <td>  {{ App\Kpi\SolveKpi::forUser( auth()->user() ) }}        </td>
+            <td>  {{ App\Kpi\OneTouchResolutionKpi::forUser( auth()->user() ) }} %        </td>
+            <td>  {{ App\Kpi\ReopenedKpi::forUser( auth()->user() ) }} %        </td>
+        </tr>
+    </table>
 
 @endsection
