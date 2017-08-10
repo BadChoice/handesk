b911dde6c0a88e51b67b09962b477e2310204815
Jordi Puigdellívol <jordi@gloobus.net>
2017-08-10 23:11:40 +0200

    Display the nº of comments/tickets when running the poll from email

--- app/Console/Commands/CreateTicketsFromNewEmails.php
+++ app/Console/Commands/CreateTicketsFromNewEmails.php
@@ -9,7 +9,9 @@ class CreateTicketsFromNewEmails extends Command{
     protected $description  = 'Fetches the INBOX emails in the configured account, and creates tickets for them';
 
     public function handle() {
-        dispatch( new \App\Jobs\CreateTicketsFromNewEmails() );
-        $this->info("Done");
+        $job = new \App\Jobs\CreateTicketsFromNewEmails();
+        dispatch( $job ) ;
+        $this->info("New Tickets: {$job->newTickets}");
+        $this->info("New Comments: {$job->newComments}");
     }
 }
--- app/Jobs/CreateTicketsFromNewEmails.php
+++ app/Jobs/CreateTicketsFromNewEmails.php
@@ -5,7 +5,6 @@ namespace App\Jobs;
 use App\Services\Pop3\Pop3;
 use App\Services\Pop3\Pop3MessageCommentParser;
 use App\Ticket;
-use App\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
@@ -15,6 +14,9 @@ use Illuminate\Foundation\Bus\Dispatchable;
 class CreateTicketsFromNewEmails implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $newComments = 0;
+    public $newTickets  = 0;
 
     public function handle(Pop3 $pop3) {
         $pop3->login( config('mail.fetch.host'), config('mail.fetch.port'), config('mail.fetch.username'), config('mail.fetch.password') );
@@ -28,6 +30,7 @@ class CreateTicketsFromNewEmails implements ShouldQueue
     private function processMessage($message){
         if( $this->addCommentFromMessage( $message ) ) return;
         Ticket::createAndNotify($message->from(), $message->subject(), $message->body(), ["email"]);
+        $this->newTickets++;
     }
 
     private function addCommentFromMessage( $message ){
@@ -36,6 +39,7 @@ class CreateTicketsFromNewEmails implements ShouldQueue
         if( ! $ticket ) return false;
         $ticket->addComment( $messageParser->getUser( $ticket ),
                              $messageParser->getCommentBody() );
+        $this->newComments++;
         return true;
     }
 }
