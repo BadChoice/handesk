14ca0c74de15f9c5624466c22a2f267b68e3fda7
Jordi Puigdellívol <jordi@gloobus.net>
2017-08-11 15:36:26 +0200

    can add/remove tags from leads

---
+++ app/Http/Controllers/LeadTagsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Lead;
+
+class LeadTagsController extends Controller
+{
+    public function store(Lead $lead){
+        $lead->attachTags([request('tag')]);
+        return response()->json();
+    }
+
+    public function destroy(Lead $lead, $tag){
+        $lead->detachTag( $tag );
+        return response()->json();
+    }
+}
