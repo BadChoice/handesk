a3f007b946c2465beda446a10021100713718d33
Jordi Puigdellívol <jordi@gloobus.net>
2017-08-12 16:34:51 +0200

    made team create equal to team edit

--- resources/views/teams/create.blade.php
+++ resources/views/teams/create.blade.php
@@ -2,15 +2,16 @@
 @section('content')
     <div class="description">
         <a href="{{ url()->previous() }}">Teams</a>
+    </div>
+    <div class="comment description actions mb4">
         <h3>New Team</h3>
     </div>
-
     {{ Form::open(["url" => route('teams.store')]) }}
     <table class="w-50">
         <tr><td>{{ __("team.name") }}: </td><td><input class="w-100" name="name"  >                         </td></tr>
         <tr><td>{{ __("team.email") }}: </td><td><input class="w-100" name="email">                         </td></tr>
         <tr><td>{{ __("team.slack_webhook_url") }}:</td><td> <input class="w-100" name="slack_webhook_url" ></td></tr>
-        <tr><td colspan="2"> <button class="uppercase"> @busy {{ __('team.new') }}</button>                 </td></tr>
+        <tr><td colspan="2"> <button class="ph4 uppercase"> @busy {{ __('team.new') }}</button>                 </td></tr>
     </table>
     {{ Form::close() }}
 
