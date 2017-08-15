8a9a18e3dbcd26be64483b117ecd615785a022c9
Jordi Puigdellívol <jordi@gloobus.net>
2017-08-15 16:12:25 +0200

    added tests for register with team token

--- tests/Feature/Back/BackTest.php
+++ tests/Feature/Back/BackTest.php
@@ -201,4 +201,36 @@ class BackTest extends TestCase
             $this->assertTrue($ticket->tags->pluck('name')->contains('second tag'));
         });
     }
+
+    /** @test */
+    public function a_user_can_register(){
+        $team = factory(Team::class)->create(["token" => "TEAMTOKEN"]);
+
+        $response = $this->post('register',[
+            "name"                  => "Peter parker",
+            "email"                 => "peter@parker.com",
+            "password"              => "secret",
+            "password_confirmation" => "secret",
+            "team_token"            => "TEAMTOKEN",
+        ]);
+
+        $response->assertStatus(Response::HTTP_FOUND);
+        $this->assertTrue( $team->members->contains(function($member){
+            return $member->name = "Peter parker";
+        }));
+    }
+
+    /** @test */
+    public function can_not_register_without_a_valid_token(){
+        $response = $this->post('register',[
+            "name"                  => "Peter parker",
+            "email"                 => "peter@parker.com",
+            "password"              => "secret",
+            "password_confirmation" => "secret",
+            "team_token"            => "NON_EXISTING_TOKEN",
+        ]);
+        $response->assertStatus(Response::HTTP_FOUND);
+        $response->assertSessionHasErrors("team_token");
+        $this->assertEquals(0, User::count() );
+    }
 }
