9342e6efe96edd6a3fecffce19aae78cb9a75d13
Jordi Puigdellívol <jordi@gloobus.net>
2017-08-16 10:09:16 +0200

    updatding default name

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Handesk') }}</title>
 
     <!-- Styles -->
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
