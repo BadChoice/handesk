3ae221cbf91afc86f53a3a659618d6ea5b9bf4e3
Jordi Puigdellívol <jordi@gloobus.net>
2017-08-21 15:49:22 +0200

    improved attachment

--- resources/views/components/attachments.blade.php
+++ resources/views/components/attachments.blade.php
@@ -1,9 +1,8 @@
 @if($attachments->count() )
-    <div class="mt1">
+    <div class="mt2">
         @foreach( $attachments as $attachment)
             @icon(paperclip)
             <a href="{{ asset("storage/attachments/$attachment->path")}}" target="_blank">{{ $attachment->path }}</a>
-            &nbsp;
         @endforeach
     </div>
 @endif
