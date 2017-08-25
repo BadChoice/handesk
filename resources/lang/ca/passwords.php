90b50dde7317a4ed3f44c3ba3bbe8289764fea74
Pau <pau@revo.works>
2017-08-25 10:24:47 +0200

    all translations except validation

---
+++ resources/lang/ca/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'La contrassenya a de tenir almenys sis caràcters i coincidir amb la confirmació.',
+    'reset' => 'La teva contrassenya ha estat restablerta!',
+    'sent' => "T'hem enviat un email amb un link per restablir la teva contrassenya!",
+    'token' => 'El token per restablir la contrassenya és invàlid.',
+    'user' => "No trobem cap usuari amb aquest email.",
+
+];
---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'La contraseña debe tener al menos seis carácteres y debe coincidir con la contraseña de confirmación.',
+    'reset' => 'Tu contraseña ha sido restablecida!',
+    'sent' => "Te hemos enviado un email con un link para restablecer tu contraseña!",
+    'token' => 'El token para restablecer tu contraseña es inválido.',
+    'user' => "No encontramos ningún usuario con este email.",
+
+];
