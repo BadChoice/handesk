0d685aaf9d05b494ba48615c503dd005a8363c61
Deleu <deleugyn@gmail.com>
2017-09-07 15:15:25 -0400

    Remove unused code

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,15 +8,4 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-
-    protected function disableExceptionHandling()
-    {
-        app()->instance(Handler::class, new class extends Handler {
-            public function __construct() {}
-            public function report(\Exception $e) { }
-            public function render($request, \Exception $e) {
-                throw $e;
-            }
-        });
-    }
 }
