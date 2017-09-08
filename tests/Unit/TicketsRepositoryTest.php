b180b3aed56ec671b92cb89c6541016eb0e2f552
Deleu <deleugyn@gmail.com>
2017-09-08 04:32:39 -0400

    Skip not implemented yet tests

--- tests/Unit/TicketsRepositoryTest.php
+++ tests/Unit/TicketsRepositoryTest.php
@@ -14,36 +14,36 @@ class TicketsRepositoryTest extends TestCase
 
     /** @test */
     public function can_get_all_tickets_for_admin(){
-
+        $this->markTestSkipped('Not implemented yet');
     }
 
     /** @test */
     public function can_get_all_tickets_for_non_admin(){
-
+        $this->markTestSkipped('Not implemented yet');
     }
 
     /** @test */
     public function can_get_assigned_tickets(){
-
+        $this->markTestSkipped('Not implemented yet');
     }
 
     /** @test */
     public function can_get_unassigned_tickets_for_admin(){
-
+        $this->markTestSkipped('Not implemented yet');
     }
 
     /** @test */
     public function can_get_unassigned_tickets_for_non_admin(){
-
+        $this->markTestSkipped('Not implemented yet');
     }
 
     /** @test */
     public function can_get_archived_tickets(){
-
+        $this->markTestSkipped('Not implemented yet');
     }
     
     /** @test */
     public function can_get_recently_updated_tickets(){
-                
+        $this->markTestSkipped('Not implemented yet');
     }
 }
