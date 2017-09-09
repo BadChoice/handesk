0b4190a20ddef5da78fc274ee6b6fbefe0489ddf
Deleu <deleugyn@gmail.com>
2017-09-09 05:02:50 -0400

    Moving eloquent tests into Unit Tests

--- tests/Unit/Eloquent/AdminTest.php
+++ tests/Unit/Eloquent/AdminTest.php
@@ -1,38 +1,36 @@
 <?php
 
-namespace Tests\Eloquent;
+namespace Tests\Unit\Eloquent;
 
 use App\Authenticatable\Admin;
-use App\Authenticatable\Assistant;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class AssistantTest extends TestCase
+class AdminTest extends TestCase
 {
     use RefreshDatabase;
 
     /**
-     * @var Assistant
+     * @var Admin
      */
-    protected $assistant;
+    protected $admin;
 
     protected function setUp()
     {
         parent::setUp();
 
-        $this->assistant = create(Assistant::class);
+        $this->admin = create(Admin::class);
     }
 
     /**
      * @test
-     * @group f
      */
-    public function assistant_does_not_include_users()
+    public function admin_does_not_include_users()
     {
         create(User::class);
 
-        $this->assertEquals(1, Assistant::count());
+        $this->assertEquals(1, Admin::count());
         $this->assertEquals(2, User::count());
     }
 }
--- tests/Unit/Eloquent/AssistantTest.php
+++ tests/Unit/Eloquent/AssistantTest.php
@@ -1,36 +1,37 @@
 <?php
 
-namespace Tests\Eloquent;
+namespace Tests\Unit\Eloquent;
 
 use App\Authenticatable\Admin;
+use App\Authenticatable\Assistant;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class AdminTest extends TestCase
+class AssistantTest extends TestCase
 {
     use RefreshDatabase;
 
     /**
-     * @var Admin
+     * @var Assistant
      */
-    protected $admin;
+    protected $assistant;
 
     protected function setUp()
     {
         parent::setUp();
 
-        $this->admin = create(Admin::class);
+        $this->assistant = create(Assistant::class);
     }
 
     /**
      * @test
      */
-    public function admin_does_not_include_users()
+    public function assistant_does_not_include_users()
     {
         create(User::class);
 
-        $this->assertEquals(1, Admin::count());
+        $this->assertEquals(1, Assistant::count());
         $this->assertEquals(2, User::count());
     }
 }
