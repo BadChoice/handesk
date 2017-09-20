cffc7991b2bf75e871b57721bf1807b347b3ca72
Badchoice <jordi@gloobus.net>
2017-09-20 16:52:21 +0200

    changed instanceof to ticketevent

--- app/TicketEvent.php
+++ app/TicketEvent.php
@@ -4,18 +4,23 @@ namespace App;
 
 class TicketEvent extends BaseModel
 {
-    public static function make($ticket, $description){
-        $ticket->events()->create([
-            "user_id" => auth()->user()->id ?? null,
-            "body" => $description
-        ]);
+    public static function make($ticket, $description)
+    {
+        if($ticket instanceof Ticket) {
+            $ticket->events()->create([
+                "user_id" => auth()->user()->id ?? null,
+                "body"    => $description
+            ]);
+        }
     }
 
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function author(){
+    public function author()
+    {
         return $this->user;
     }
 }
