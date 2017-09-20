932c9d5a04c15369d4cb68389a54adb49c5bf7b5
Badchoice <jordi@gloobus.net>
2017-09-20 16:53:10 +0200

    removed unused vars

--- app/Services/FakeIssueCreator.php
+++ app/Services/FakeIssueCreator.php
@@ -4,10 +4,9 @@ namespace App\Services;
 
 class FakeIssueCreator implements IssueCreator
 {
-    public $resource_uri = "https://fakeissuer.com/issue/1";
-    public $local_id = 1;
 
-    public function createIssue($repository, $title, $body){
+    public function createIssue($repository, $title, $body)
+    {
         return (object)[
             "resource_uri" => "https://fakeissuer.com/issue/1",
             "local_id" => 1,
