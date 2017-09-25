ef78c1af7cfae9a413aab4aaf5e044905ba84b39
Ronaldo Paggi <ronaldo.paggi@gmail.com>
2017-09-25 15:49:11 -0300

    Translate passwords.php

--- resources/lang/ptbr/passwords.php
+++ resources/lang/ptbr/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset'    => 'Your password has been reset!',
-    'sent'     => 'We have e-mailed your password reset link!',
-    'token'    => 'This password reset token is invalid.',
-    'user'     => "We can't find a user with that e-mail address.",
+    'password' => 'A senha deve ter pelo menos seis caracteres e ser igual a sua confirmação.',
+    'reset'    => 'Sua senha foi resetada!',
+    'sent'     => 'Nós enviamos ao seu email um link para resetar sua senha!',
+    'token'    => 'Este token de reset de senha é inválido.',
+    'user'     => "Nós não podemos encontrar um usuário com este e-mail.",
 
 ];
