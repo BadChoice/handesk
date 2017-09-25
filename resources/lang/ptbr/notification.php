63d7ece96baa8a16920aaeade6c160ab4353af0b
Ronaldo Paggi <ronaldo.paggi@gmail.com>
2017-09-25 16:10:09 -0300

    Translate ticket.php

--- resources/lang/ptbr/notification.php
+++ resources/lang/ptbr/notification.php
@@ -2,12 +2,12 @@
 
 return [
     'newLeadCreatedDesc'    => 'Um novo lead foi criado',
-    'leadAssigned'          => 'Lead associado',
+    'leadAssigned'          => 'Lead atribuído',
     'newTicketCreated'      => 'Novo ticket criado',
     'newTicket'             => 'Novo ticket',
     'ticketCreated'         => 'Ticket criado',
-    'ticketAssigned'        => 'Ticket associado',
-    'ticketAssignedTo'      => 'Ticket associado à',
+    'ticketAssigned'        => 'Ticket atribuído',
+    'ticketAssignedTo'      => 'Ticket atribuído à',
     'ticketUpdated'         => 'Ticket atualizado',
     'ticketEscalated'       => 'Ticket escalado',
 ];
