8bfa84b2933bd44ecd5ce2b4198916a3d18fb605
Jordi Puigdellívol <jordi.p@revo.works>
2017-11-06 13:12:10 +0100

    added missing roadmap controller

---
+++ app/Http/Controllers/RoadmapController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Idea;
+
+class RoadmapController extends Controller
+{
+    public function index(){
+        $ideasQuery = Idea::roadmap();
+        if(request('repository')){
+            $ideasQuery->where('repository', request('repository'));
+        }
+        return view('roadmap.index', ['ideas' => $ideasQuery->get()->groupBy('status') ]);
+    }
+}
