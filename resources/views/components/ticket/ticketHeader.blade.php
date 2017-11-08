0560b6012d16ba571d4de7487a3a153341cb54b4
Jordi Puigdellívol <jordi.p@revo.works>
2017-11-08 09:46:47 +0100

    showing bug icon if issue has bug

--- resources/views/components/ticket/ticketHeader.blade.php
+++ resources/views/components/ticket/ticketHeader.blade.php
@@ -3,6 +3,7 @@
                 <input type="checkbox" name="selected[{{$ticket->id}}]" meta:index="{{$ticket->id}}" class="hidden selector">
                 <span class="label ticket-status-{{ $ticket->statusName() }}">{{ str_limit(__('ticket.' . $ticket->statusName()), 1, '') }}</span>&nbsp;
                 @if( $ticket->isEscalated() ) @icon(flag) @endif
+                @if( $ticket->getIssueId() ) @icon(bug) @endif
                 <a href="{{ route('tickets.show', $ticket) }}"> #{{ $ticket->id }}. {{  str_limit($ticket->title, 35) }}</a>
         </td>
         <td> {{ $ticket->requester->name            }}</td>
