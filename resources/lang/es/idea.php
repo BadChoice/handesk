53742e36b545437b9ad61c5429253bbf171256b0
Jordi Puigdellívol <jordi.p@revo.works>
2017-11-09 15:37:14 +0100

    fixed hipervínculo

--- resources/lang/es/idea.php
+++ resources/lang/es/idea.php
@@ -15,7 +15,7 @@ Tu ticket se ha cerrado como incidencia y ha generado una sugerencia que irá di
 
 Todas las aportaciones depositadas en el Banco de Ideas REVO, serán analizadas por un Comité Multidisciplinar (compuesto por personas de nuestros departamentos de Desarrollo - Soporte - Comercial), que será el que valore la viabilidad de su implementación.
 
-Siempre que lo desees, podrás consultar nuestra Hoja de Ruta de Desarrollo (hipervínculo), con la relación actualizada de todas las propuestas aprobadas y la fecha estimada en la que se prevé cada implementación.
+Siempre que lo desees, podrás consultar nuestra Hoja de Ruta de Desarrollo, con la relación actualizada de todas las propuestas aprobadas y la fecha estimada en la que se prevé cada implementación.
 
 Gracias por permitirnos crecer contigo. Vuestras aportaciones son nuestro capital más valioso para mejorar.
 
