be738d229ec8fcc8db32227c5b41c4d702065de3
Robin Dirksen <robin@famdirksen.nl>
2018-03-28 13:13:33 +0200

    create nl/team.php

---
+++ resources/lang/nl/team.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'team'                  => 'Team|Teams',
+    'member'                => 'Lid|Liden',
+    'invitationLink'        => 'Uitnodigingslink',
+    'invitationLinkDesc'    => "Nodig meer agenten uit voor dit team door deze link te verzenden. Als ze nog geen Handesk-account hebben, gebruikt u de registratielink",
+    'join'                  => 'Deelnemen',
+    'invitedTitle'          => 'U bent uitgenodigd om deel te nemen aan het :team team',
+    'invitedDesc'           => 'Door lid te worden van dit team, kun je de support tickets zien en updaten, en ze toewijzen aan andere teamleden',
+    'register'              => 'Of verzend deze link als de gebruiker nog geen Handesk-account is',
+    'new'                   => 'Nieuw',
+    'name'                  => 'Naam',
+    'email'                 => 'Email',
+    'slack_webhook_url'     => 'Slack webhook url',
+    'slack'                 => 'Slack',
+    'slack_webhook_urlDesc' => 'Je kunt het kanaal of de gebruiker aanpassen door het toe te voegen aan het einde van de URL voorafgaand aan een? Voorbeeld: https://hooks.slack.com/services/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?#custom-channel',
+    'none'                  => 'Geen',
+];
