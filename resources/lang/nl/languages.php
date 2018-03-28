d74dba63af7b0b92f577e4e63ce88e8fe0d41206
Robin Dirksen <robin@famdirksen.nl>
2018-03-28 13:06:43 +0200

    create nl/languages.php

---
+++ resources/lang/nl/languages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'en'   => 'English',
+    'es'   => 'Spanish',
+    'ca'   => 'Catalan',
+    'fr'   => 'French',
+    'de'   => 'German',
+    'nl'   => 'Nederlands',
+    'ptbr' => 'Brazillian Portuguese',
+];
