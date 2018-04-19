41db46a279133b5aee042a112f1e5ad399318a72
Jordi Puigdellívol <jordi.p@revo.works>
2018-04-19 09:30:35 +0200

    updated log level

--- config/app.php
+++ config/app.php
@@ -120,9 +120,9 @@ return [
     |
     */
 
-    'log' => env('APP_LOG', 'single'),
+    'log' => env('APP_LOG', 'daily'),
 
-    'log_level' => env('APP_LOG_LEVEL', 'debug'),
+    'log_level' => env('APP_LOG_LEVEL', 'error'),
 
     /*
     |--------------------------------------------------------------------------
