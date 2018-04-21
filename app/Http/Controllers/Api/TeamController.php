0d332d95cd8e300220249629de1fc51a274708ed
BadChoice <jordi@gloobus.net>
2018-04-21 09:13:00 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Api/TeamController.php
+++ app/Http/Controllers/Api/TeamController.php
@@ -8,16 +8,16 @@ class TeamController extends ApiController
 {
     public function store()
     {
-        $this->validate(request(),[
-            "name" => "required|min:3",
-            "email" => "required|email",
+        $this->validate(request(), [
+            'name'  => 'required|min:3',
+            'email' => 'required|email',
         ]);
 
         return $this->respond(Team::create([
             'name'              => request('name'),
             'email'             => request('email'),
             'slack_webhook_url' => request('slack_webhook_url'),
-            'token'             => str_random(24)
+            'token'             => str_random(24),
         ]), 201);
     }
 }
