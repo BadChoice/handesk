56f56e115c7e88540d88026c499561c76418f695
Robin Dirksen <robin@famdirksen.nl>
2018-04-27 12:15:48 +0200

    fixed styleci - https://styleci.io/analyses/qMOKwj

--- app/Http/Middleware/ForceHttps.php
+++ app/Http/Middleware/ForceHttps.php
@@ -11,6 +11,7 @@ class ForceHttps
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
+     * 
      * @return mixed
      */
     public function handle($request, Closure $next)
