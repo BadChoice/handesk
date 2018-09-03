6dc2d521c3b767e271e4e10b8ac6186deed43e53
SceLPI <cs.luizeduardo@hotmail.com>
2018-09-03 11:35:42 -0300

    New Section Ideas (Translation)

---
+++ resources/lang/ptbr/idea.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'idea'              => 'Ideia|Ideias',
+    'pendingIdea'       => 'Ideia Pendente|Ideias Pendentes',
+    'score'             => 'Pontuação',
+    'repository'        => 'Repositório',
+    'roadmap'           => 'Trilha',
+    'developmentEffort' => 'Esforço de desenvolvimento',
+    'salesImpact'       => 'Impacto de Vendas',
+    'currentImpact'     => 'Impacto Atual',
+    'edit'              => 'Editar',
+    'fromTicket'        => 'Notification | REVO Ideas bucket:
+Your ticket has been closed as an issue, and it has been created as an idea into REVO Ideas bucket.
+All the ideas will be analized by REVO team, and evaluated its viability.
+Thank you to let us grow with you..
+El Equipo de REVO',
+    'save'     => 'Salvar',
+    'new'      => 'Novo',
+    'open'     => 'Aberto',
+    'closed'   => 'Fechado',
+    'solved'   => 'Resolvido',
+    'accepted' => 'Aceito',
+    'declined' => 'Recusado',
+    'dueDate'  => 'Vencido',
+];
