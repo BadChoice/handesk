27cf507c15821dd049de695f2e0be6aff5470371
Jordi Puigdellívol <jordi@gloobus.net>
2018-11-09 12:25:40 +0100

    Impreoved rating email

--- resources/views/emails/rateTicket.blade.php
+++ resources/views/emails/rateTicket.blade.php
@@ -1,19 +1,30 @@
 @extends('emails.layout')
 
+<style>
+    a{
+        text-decoration: none;
+    }
+    a.ratingStar{
+        font-size:24px;
+        color: #F6C243;
+        margin-right:5px;
+    }
+</style>
+
 @section('body')
         <div style="border-bottom:1px solid #efefef; padding-bottom:10px;">
             <span style="font-size:12px">{{ $title }}</span>
         </div>
 
-        <div style="border-bottom:1px solid #efefef; padding-bottom:10px; margin-left:20px; margin-top:20px;">
-            {{ $ticket->title }} <br>
+        <div style="border-bottom:1px solid #efefef; padding-bottom:30px; margin-left:20px; margin-top:20px;">
+            {{ $ticket->title }} <br><br>
             {{__('notification.rateTicketDesc')}}
-            <br>
-            <a href="{{$url}}?rating=1">1</a>
-            <a href="{{$url}}?rating=2">2</a>
-            <a href="{{$url}}?rating=3">3</a>
-            <a href="{{$url}}?rating=4">4</a>
-            <a href="{{$url}}?rating=5">5</a>
+            <br><br>
+            <a class="ratingStar" href="{{$url}}?rating=1">☆</a>
+            <a class="ratingStar" href="{{$url}}?rating=2">☆</a>
+            <a class="ratingStar" href="{{$url}}?rating=3">☆</a>
+            <a class="ratingStar" href="{{$url}}?rating=4">☆</a>
+            <a class="ratingStar" href="{{$url}}?rating=5">☆</a>
         </div>
 
         <span style="color:white">ticket-id:{{$ticket->id}}.</span>
