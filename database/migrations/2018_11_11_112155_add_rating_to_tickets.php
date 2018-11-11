4dca0f9b59287561c47d0e829b859a56b141dc32
Daniel Winter <hallo@danielwinter.at>
2018-11-11 12:22:47 +0100

    Created a new Migration for Rating on Tickets

---
+++ database/migrations/2018_11_11_112155_add_rating_to_tickets.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRatingToTickets extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->tinyInteger('rating')->after('level')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->dropColumn('rating');
+        });
+    }
+}
