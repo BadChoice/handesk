2bdedde15d4e8f1e66430a985ff38eb45e400293
Jordi Puigdellívol <jordi@gloobus.net>
2019-02-26 16:11:33 +0100

    Updated tests to use the new bitbucket api

--- tests/Feature/Back/Tickets/TicketEventsTest.php
+++ tests/Feature/Back/Tickets/TicketEventsTest.php
@@ -77,12 +77,12 @@ class TicketEventsTest extends TestCase
     public function creating_ticket_issue_creates_a_ticket_event(){
         $ticket = factory(Ticket::class)->create();
 
-        $ticket->createIssue( new FakeIssueCreator, null );
+        $ticket->createIssue( new FakeIssueCreator, 'fake/repo' );
 
         $this->assertCount(1, $ticket->fresh()->events);
         tap($ticket->fresh()->events->first(), function($event){
             $this->assertEquals($this->agent->id, $event->user->id );
-            $this->assertEquals("Issue created #1 at ", $event->body);
+            $this->assertEquals("Issue created #1 at fake/repo", $event->body);
         });
     }
 
