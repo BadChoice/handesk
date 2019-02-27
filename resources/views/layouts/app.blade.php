fd31e61209ada8d6c66fafa8fdc25c618145992f
loveun <loveun1988@outlook.com>
2019-02-27 10:52:48 -0400

    update css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/all.css') }}" rel="stylesheet">
     <link href="{{ asset('css/switcher.css') }}" rel="stylesheet">
-
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix');
+let mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,17 +11,25 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.babel([
-        //'resources/assets/js/app.js',
-        'vendor/badchoice/thrust/src/resources/js/thrust.min.js',
-        'resources/assets/js/utils.js',
-        'resources/assets/js/libs/jquery.tagsinput.min.js',  //http://xoxco.com/projects/code/tagsinput/
-        'resources/assets/js/libs/mention.js/bootstrap-typeahead.js',  //https://github.com/ivirabyan/jquery-mentions
-        'resources/assets/js/libs/mention.js/mention.js',  //https://github.com/ivirabyan/jquery-mentions
-    ], 'public/js/app.js')
-    .babel('resources/assets/js/moment.min.js'                 ,'public/js/moment.js')
-    .less('resources/assets/less/style.less',                  '../resources/assets/css/style.css')
-    .styles([
-        'resources/assets/css/libs/jquery.tagsinput.min.css',
-        'resources/assets/css/style.css'
-    ],'public/css/all.css');
+mix
+  .babel(
+    [
+      //'resources/assets/js/app.js',
+      "vendor/badchoice/thrust/src/resources/js/thrust.min.js",
+      "resources/assets/js/utils.js",
+      "resources/assets/js/libs/jquery.tagsinput.min.js", //http://xoxco.com/projects/code/tagsinput/
+      "resources/assets/js/libs/mention.js/bootstrap-typeahead.js", //https://github.com/ivirabyan/jquery-mentions
+      "resources/assets/js/libs/mention.js/mention.js" //https://github.com/ivirabyan/jquery-mentions
+    ],
+    "public/js/app.js"
+  )
+  .babel("resources/assets/js/moment.min.js", "public/js/moment.js")
+  .less("resources/assets/less/style.less", "../resources/assets/css/style.css")
+  .styles(
+    [
+      "resources/assets/css/libs/jquery.tagsinput.min.css",
+      "resources/assets/css/style.css"
+    ],
+    "public/css/all.css"
+  )
+  .sass("resources/sass/app.scss", "public/css");
