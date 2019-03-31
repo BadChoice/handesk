6a6f4fde91f46c61a10cf5003e844cfaa23f0048
Jordi Puigdellívol <jordi@gloobus.net>
2019-03-31 16:46:37 +0200

    Thrust metrics working

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Thrust\Metrics\NewTicketsMetric;
 use App\Thrust\Metrics\SolvedMetric;
 use App\Thrust\Metrics\TeamTicketsMetric;
+use App\Thrust\Metrics\TicketsCountMetric;
 use App\Thrust\Metrics\TicketTypeMetric;
 use Carbon\Carbon;
 use App\Repositories\KpiRepository;
@@ -22,6 +23,7 @@ class ReportsController extends Controller
     public function analytics(){
         return view('reports.analytics',[
            'metrics' => [
+               (new TicketsCountMetric),
                (new SolvedMetric),
                (new NewTicketsMetric),
                (new TicketTypeMetric),
