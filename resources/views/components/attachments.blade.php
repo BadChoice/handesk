d772fd255f88d61ddd5fb3a02b2522fe8d8ee83a
Jordi Puigdellívol <jordi@gloobus.net>
2019-04-08 12:02:10 +0200

    Making attachments work with google

--- resources/views/components/attachments.blade.php
+++ resources/views/components/attachments.blade.php
@@ -2,7 +2,11 @@
     <div class="mt2">
         @foreach( $attachments as $attachment)
             @icon(paperclip)
-            <a href="{{ Storage::url("attachments/$attachment->path")}}" target="_blank">{{ $attachment->path }}</a>
+            @if (Storage::getDefaultDriver() == 'google')
+                <a href="{{ config('filesystems.disks.google.endpoint') . '/' . config('filesystems.disks.google.bucket') . "/public/attachments/$attachment->path" }}" target="_blank">{{ $attachment->path }}</a>
+            @else
+                <a href="{{ Storage::url("attachments/$attachment->path")}}" target="_blank">{{ $attachment->path }}</a>
+            @endif
         @endforeach
     </div>
 @endif
