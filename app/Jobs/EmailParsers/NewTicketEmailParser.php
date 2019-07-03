7cbc37839cc5a8b134508b27ab78b3a602bc0ae3
EatelServers <servers@eatel.net>
2019-07-03 19:13:23 +0000

    Updates from local server-cca2697

--- app/Jobs/EmailParsers/NewTicketEmailParser.php
+++ app/Jobs/EmailParsers/NewTicketEmailParser.php
@@ -9,7 +9,7 @@ class NewTicketEmailParser
 {
     public function handle($message)
     {
-        $ticket = Ticket::createAndNotify(['name' => $message->fromName, 'email' => $message->fromAddress], $message->subject, $message->body(), ['email']);
+        $ticket = Ticket::createAndNotify(['name' => $message->fromName, 'email' => $message->fromAddress], $message->subject, $message->body(), ['email'], 1);
         Attachment::storeAttachmentsFromEmail($message, $ticket);
 
         return true;
