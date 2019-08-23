efe1d2b7e89ae06319eb788d723d35be89683ccf
Jordi Puigdellívol <jordi@gloobus.net>
2019-08-23 09:13:37 +0200

    Some fixes

--- resources/views/components/ticket/actions.blade.php
+++ resources/views/components/ticket/actions.blade.php
@@ -1,4 +1,9 @@
     @if($ticket->status == App\Ticket::STATUS_SOLVED)
+        @if($ticket->getIssueId())
+            <div class="float-right" style="margin-top:-20px; margin-right:60px;">
+                @include('components.ticket.issue')
+            </div>
+        @endif
         <div class="float-right mt-4 mr4 ml-3">
             {{ Form::open(["url" => route('tickets.reopen', $ticket)]) }}
             <button class="button fs1 secondary mt-1"> {{ __('ticket.reopen') }}</button>
