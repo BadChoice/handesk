520be0be4486c3d7a9b55319d230f7880c49c113
Daniele Scasciafratte <mte90net@gmail.com>
2020-02-10 19:43:50 +0100

    fix #308

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ Route::group(['namespace' => 'Api', 'middleware' => 'apiAuth'], function () {
     Route::resource('tickets', 'TicketsController', ['except' => 'destroy']);
     Route::post('tickets/{ticket}/comments', 'CommentsController@store');
     Route::post('tickets/{ticket}/assign', 'TicketAssignController@store');
-    Route::gpost('users/create', 'UsersController@store');
+    Route::post('users/create', 'UsersController@store');
     Route::post('teams', 'TeamController@store');
     Route::get('users', 'UsersController@index');
     Route::get('teams/{team}/tickets', 'TeamTicketsController@index');
