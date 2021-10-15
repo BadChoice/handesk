d5bac936c7727e49aeb66d8de12227f0039f5904
Dave Gill <david.g.h.gill@gmail.com>
2021-10-15 15:52:01 +0100

    Simplified ticket view with attachements to remove reliance on Str

--- resources/views/components/attachments.blade.php
+++ resources/views/components/attachments.blade.php
@@ -2,14 +2,7 @@
     <div class="mt2">
         @foreach( $attachments as $attachment)
             @icon(paperclip)
-            @if (Storage::getDefaultDriver() == 'google')
-                <a href="{{ config('filesystems.disks.google.endpoint') . '/' . config('filesystems.disks.google.bucket') . "/public/attachments/$attachment->path" }}" target="_blank">{{ $attachment->path }}</a>
-            @else
-                @if (Str::endsWith($attachment->path, ['.png', '.jpg', '.pdf', '.mov', '.mp4']))
-                    <a href="{{ route('attachments', $attachment->path) }}" target="_blank">{{ $attachment->path }}</a>
-                @else
-                    <a href="{{ Storage::url("attachments/$attachment->path")}}" target="_blank">{{ $attachment->path }}</a>
-                @endif
+                <a href="{{ Storage::url("attachments/$attachment->path")}}" target="_blank">{{ $attachment->path }}</a>
             @endif
         @endforeach
     </div>
