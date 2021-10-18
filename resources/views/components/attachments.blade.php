dd4a35475de05e8d234b799f1dbdf9f6a6c4b4df
David Broad <davidbroad@servicecertainty.co.uk>
2021-10-18 13:16:24 +0100

    Removed stray @endif

--- resources/views/components/attachments.blade.php
+++ resources/views/components/attachments.blade.php
@@ -3,7 +3,6 @@
         @foreach( $attachments as $attachment)
             @icon(paperclip)
                 <a href="{{ Storage::url("attachments/$attachment->path")}}" target="_blank">{{ $attachment->path }}</a>
-            @endif
         @endforeach
     </div>
 @endif
