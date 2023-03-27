606536f87d7ffcd221008033ad60a63a86252b09
syehan <muhammad.syehan@gmail.com>
2023-03-27 12:01:37 +0700

    fix: entrypoint docker

--- docker-config/entrypoint.sh
+++ docker-config/entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 set -e
 
-echo "* * * * * php /var/www/html/artisan schedule:run >> /dev/null 2>&1" >> /etc/crontab
-
-php /var/www/html/artisan october:migrate
-php /var/www/html/artisan horizon:install
-php /var/www/html/artisan horizon:publish
-php /var/www/html/artisan vendor:publish --provider="Laravel\Horizon\HorizonServiceProvider"
+php /var/www/html/artisan migrate --seed
 
 service nginx start
 supervisord -c /etc/supervisord.conf
