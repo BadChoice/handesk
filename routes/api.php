9b0bcee640a2bd75159cb49937a8b4088b8e075b
agifsofyan <alpenizme@gmail.com>
2023-04-01 23:25:42 +0700

    route prefix to v1 and disable the token response in user list

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,5 @@
 <?php
 
-Route::group(['namespace' => 'Api', 'middleware' => 'apiAuth'], function () {
-    Route::resource('tickets', 'TicketsController', ['except' => 'destroy']);
-    Route::post('tickets/{ticket}/comments', 'CommentsController@store');
-    Route::post('tickets/{ticket}/assign', 'TicketAssignController@store');
-    Route::get('users', 'UsersController@index');
-    Route::post('users/create', 'UsersController@store');
-    Route::post('teams', 'TeamController@store');
-    Route::get('teams/{team}/tickets', 'TeamTicketsController@index');
-    Route::get('teams/{team}/leads', 'TeamLeadsController@index');
-
-    Route::resource('leads', 'LeadsController', ['only' => 'store']);
-    Route::resource('ideas', 'IdeasController', ['only' => ['store', 'index']]);
-});
-
 Route::post('agent/login', 'Api\AgentController@login');
 Route::group(['namespace' => 'Api', 'prefix' => 'agent', 'middleware' => 'apiAuthAgent'], function () {
     Route::resource('tickets', 'AgentController', ['only' => 'index']);
@@ -37,4 +23,20 @@ Route::group(['namespace' => 'Api', 'prefix' => 'v1', 'middleware' => 'auth:api'
     Route::get('ticket/detail/{id}', 'AgentTicketCommentsController@detail');
     Route::post('ticket/start-task/{id}', 'AgentTicketCommentsController@startTask');
     Route::post('ticket/{id}/comments', 'AgentTicketCommentsController@report');
+
+
+    /**
+     * Original Handesk Route
+     */
+    Route::resource('tickets', 'TicketsController', ['except' => 'destroy']);
+    Route::post('tickets/{ticket}/comments', 'CommentsController@store');
+    Route::post('tickets/{ticket}/assign', 'TicketAssignController@store');
+    Route::get('users', 'UsersController@index');
+    Route::post('users/create', 'UsersController@store');
+    Route::post('teams', 'TeamController@store');
+    Route::get('teams/{team}/tickets', 'TeamTicketsController@index');
+    Route::get('teams/{team}/leads', 'TeamLeadsController@index');
+
+    Route::resource('leads', 'LeadsController', ['only' => 'store']);
+    Route::resource('ideas', 'IdeasController', ['only' => ['store', 'index']]);
 });
