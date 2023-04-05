36bceced9603866055543821d3d50ee7dc585abd
rizki-vhiweb <rizki@vhiweb.com>
2023-04-05 15:18:09 +0700

    chore: add migrations & models for master proposal

---
+++ app/Banner.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Banner extends Model
+{
+    use HasFactory;
+
+    protected $table = 'proposal_banners';
+}
